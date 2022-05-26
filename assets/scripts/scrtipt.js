269a42d9ad8fe88a8b3a0be1d7a6d91e3ea993de
Herli Khoury <herli.kh@gmail.com>
2022-05-26 09:37:06 -0300

    Get input value function

--- assets/scripts/scrtipt.js
+++ assets/scripts/scrtipt.js
@@ -1,2 +1,9 @@
 const form = document.getElementById('task-form');
 const taskList = document.getElementById('task-list');
+
+form.onsubmit = function (e){
+    e.preventDefault();
+    const inputField = document.getElementById('task-input');
+    addTask(inputField.value);
+    form.reset();
+}
